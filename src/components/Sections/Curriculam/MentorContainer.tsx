07864569d3aed8a729d6236c19f94ac3dab7d7ef
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-11-23 03:34:00 +0530

    mentor container rounded edges are added

--- src/components/Sections/Curriculam/MentorContainer.tsx
+++ src/components/Sections/Curriculam/MentorContainer.tsx
@@ -51,11 +51,11 @@ const MentorContainer: FC = () => {
   return (
     <>
       {/* Desktop View */}
-      <div className='relative bg-gradient-to-br from-[#15111C] to-[#000000] px-4 md:px-12 py-20 rounded-xl md:flex justify-around hidden z-10 drop-shadow-2xl shadow-sm shadow-[#ffffff36]'>
+      <div className='relative bg-gradient-to-br from-[#15111C] to-[#000000] px-4 md:px-12 py-20 rounded-[3rem] md:flex justify-around hidden z-10 drop-shadow-2xl shadow-sm shadow-[#ffffff36]'>
 
       <div className='top-outline absolute top-0 left-0 w-full h-[1px] bg-custom-gradient'></div>
       <div className='bottom-outline absolute bottom-0 left-0 w-full h-[1px] bg-custom-gradient'></div>
-      
+
         {/* Mentor Details */}
         <div className='flex flex-col gap-y-8 h-fit'>
           <p
