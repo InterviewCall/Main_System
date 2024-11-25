d2adf1c4359a6081c983536ba0f9575600ec5902
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-11-25 18:26:34 +0530

    hero stat section seperator width increased

--- src/components/Sections/Hero/HeroStat.tsx
+++ src/components/Sections/Hero/HeroStat.tsx
@@ -6,7 +6,7 @@ const HeroStat: FC<HeroStatProps> = ({ stats, stat, index }) => {
   return (
     <>
       {index != stats.length && index != 0 && (
-        <div className="h-20 bg-gray-500 w-[0.25px]" />
+        <div className="h-20 bg-gray-500 w-[5px]" />
       )}
       <div className="flex flex-col w-full items-center">
         <p className="text-white md:text-2xl text-sm font-bold">{stat.count}</p>
