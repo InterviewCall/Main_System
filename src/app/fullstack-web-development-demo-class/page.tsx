164ee0817c43835e905b6eb95eada7dbe813e4bc
Arijit Ganguly <aganguly158@gmail.com>
2024-11-29 21:22:55 +0530

    checking

--- src/app/fullstack-web-development-demo-class/page.tsx
+++ src/app/fullstack-web-development-demo-class/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 // import { useRouter } from 'next/navigation';
 import { FC, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
