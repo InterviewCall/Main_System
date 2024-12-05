e37c1b9a75115ab65f504287221505424acdaa5f
Arijit Ganguly <aganguly158@gmail.com>
2024-12-06 02:15:57 +0530

    job switch brochure download added

--- src/components/Sections/Alumni/DownloadBrochureButton.tsx
+++ src/components/Sections/Alumni/DownloadBrochureButton.tsx
@@ -1,11 +1,22 @@
 'use client';
 
-import { FC } from 'react';
+import { usePathname } from 'next/navigation';
+import { FC, useEffect, useState } from 'react';
 import { GoArrowDown } from 'react-icons/go';
 
 const DownloadBrochureButton: FC = () => {
+    const pathName = usePathname();
+    const [downloadBrochure, setDownloadBrochure] = useState('');
+    useEffect(() => {
+      if(pathName == 'fullstack-mern') {
+        setDownloadBrochure('brochure');
+      }
+      else if(pathName == 'job-switch') {
+        setDownloadBrochure('js-download');
+      }
+    }, [pathName]);
     return (
-      <button className='relative brochure py-2 px-4 rounded-full flex gap-5 bg-black text-white items-center border border-white download-brochure overflow-hidden'>
+      <button className={`${downloadBrochure} relative py-2 px-4 rounded-full flex gap-5 bg-black text-white items-center border border-white overflow-hidden`}>
         <span className='animate-shine absolute w-full h-full  bg-gradient-to-r from-transparent via-white to-transparent cursor-default'></span>
         <p className='ml-2 text-base font-medium uppercase'>
           Download Brochure
