f45bcf3f9365a9070660cb720b6ef4d99fcfa5ce
Arijit Ganguly <aganguly158@gmail.com>
2025-02-21 20:51:46 +0530

    request callback css changed

--- src/components/Header/HeaderButton.tsx
+++ src/components/Header/HeaderButton.tsx
@@ -7,7 +7,7 @@ const HeaderButton: FC = () => {
         window.scrollTo({ top: 0, behavior: 'smooth'});
     }
     return (
-        <button className='req-cb' onClick={scrollToTop}>
+        <button className='advisor' onClick={scrollToTop}>
         <div className='p-[3px] bg-gradient-to-b from-[#00FFE0] to-[#0B2463] rounded-full shadow-custom-multi'>
           <p className='text-white md:py-2 md:px-6 px-4 py-[10px] md:text-base text-xs whitespace-nowrap rounded-full bg-black'>
             Request Callback
