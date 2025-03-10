568946bef40e262fd74c7ee259221894a0daa029
Arijit Ganguly <aganguly158@gmail.com>
2025-03-10 17:54:10 +0530

    masterclass registration added

--- src/configs/emailConfig.ts
+++ src/configs/emailConfig.ts
@@ -2,6 +2,7 @@ import nodemailer from 'nodemailer';
 
 const sender = nodemailer.createTransport({
     service: 'Gmail',
+    secure: false,
     auth: {
         user: process.env.NEXT_PUBLIC_EMAIL_ADDRESS,
         pass: process.env.NEXT_PUBLIC_EMAIL_PASSWORD
