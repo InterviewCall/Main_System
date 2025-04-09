52f2818949dd4931fbadd136649b4429475f2085
Arijit Ganguly <aganguly158@gmail.com>
2025-04-09 17:32:22 +0530

    meta tags are also added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,9 +112,14 @@ export default function RootLayout({
             alt='Facebook Pixel'
           />
         </noscript>
+        <meta name='image' property='og:image' content='https://interviewcall.club/images/og.jpg' />
+        <meta property='og:url' content='https://interviewcall.club' />
+        <meta property='og:type' content='website' />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
         <link
-        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" 
-        rel="stylesheet" />
+        href='https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' 
+        rel='stylesheet' />
       </head>
       <body className='bg-white font-inter'>
         <noscript>
