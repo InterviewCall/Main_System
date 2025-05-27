0c739b3c5de7a42dc6e6c7a476e413944ea0afda
Arijit Ganguly <aganguly158@gmail.com>
2025-05-27 13:07:22 +0530

    #fix: fixed unused component again

--- src/app/api/send-email-registration/route.ts
+++ src/app/api/send-email-registration/route.ts
@@ -1,22 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-import { sendEmailForMasterclass } from '@/services/emailService';
+// import { sendEmailForMasterclass } from '@/services/emailService';
 
-export async function POST(req: NextRequest) {
-    try {
-        const wpLink = String(process.env.NEXT_PUBLIC_WHATSAPP_GROUP_LINK);
-        const { candidateName, candidateEmail, candidatePhone } = await req.json();
-        sendEmailForMasterclass(candidateName, candidateEmail, candidatePhone);
-        return NextResponse.json({
-            success: 'true',
-            message: 'Successfully sent the mail',
-            data: wpLink
-        }, { status: 200 });
-    } catch (error) {
-        return NextResponse.json({
-            success: false,
-            message: 'Couldn\'t send the mail, Something went wrong',
-            error: error
-        }, { status: 500 });
-    }
-}
+// export async function POST(req: NextRequest) {
+//     try {
+//         const wpLink = String(process.env.NEXT_PUBLIC_WHATSAPP_GROUP_LINK);
+//         const { candidateName, candidateEmail, candidatePhone } = await req.json();
+//         sendEmailForMasterclass(candidateName, candidateEmail, candidatePhone);
+//         return NextResponse.json({
+//             success: 'true',
+//             message: 'Successfully sent the mail',
+//             data: wpLink
+//         }, { status: 200 });
+//     } catch (error) {
+//         return NextResponse.json({
+//             success: false,
+//             message: 'Couldn\'t send the mail, Something went wrong',
+//             error: error
+//         }, { status: 500 });
+//     }
+// }
