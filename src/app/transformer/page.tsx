ba2966bd709cbb3a7af85180a0a7d48bf2c35a82
Arijit Ganguly <aganguly158@gmail.com>
2025-05-27 13:37:03 +0530

    #fix: added suspense component for routes

--- src/app/transformer/page.tsx
+++ src/app/transformer/page.tsx
@@ -1,27 +1,11 @@
-'use client';
+import { Suspense } from 'react';
 
-import { usePathname } from 'next/navigation';
+import ClientTransformerPage from '@/components/TransFormerPage/ClientTransFormerPage';
 
-import AlumniSection from '@/components/Sections/Alumni/AlumniSection';
-import CurriculamSection from '@/components/Sections/Curriculam/CurriculamSection';
-// import EventSection from '@/components/Sections/Event/EventSection';
-import FAQSection from '@/components/Sections/FAQ/FAQSection';
-import HeroSection from '@/components/Sections/Hero/HeroSection';
-import IndustrySection from '@/components/Sections/Industry/IndustrySection';
-import LearnerSection from '@/components/Sections/Learner/LearnerSection';
-import { heroSectionDetails } from '@/constant/heroSectionDetails';
-
-export default function Home() {
-  const pathName = usePathname();
+export default function JobSwitchPage() {
   return (
-    <div>
-      <HeroSection title={heroSectionDetails[pathName].title} description={heroSectionDetails[pathName].description} />
-      <AlumniSection />
-      <IndustrySection />
-      <CurriculamSection />
-      <LearnerSection />
-      {/* <EventSection /> */}
-      <FAQSection />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ClientTransformerPage />
+    </Suspense>
   );
 }
---
+++ src/components/FullstackMernPage/ClientFullstackMernPage.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+import AlumniSection from '@/components/Sections/Alumni/AlumniSection';
+import CurriculamSection from '@/components/Sections/Curriculam/CurriculamSection';
+// import EventSection from '@/components/Sections/Event/EventSection';
+import FAQSection from '@/components/Sections/FAQ/FAQSection';
+import HeroSection from '@/components/Sections/Hero/HeroSection';
+import IndustrySection from '@/components/Sections/Industry/IndustrySection';
+import LearnerSection from '@/components/Sections/Learner/LearnerSection';
+import { heroSectionDetails } from '@/constant/heroSectionDetails';
+
+export default function ClientFullstackMernPage() {
+  const pathName = usePathname();
+  return (
+    <div>
+      <HeroSection title={heroSectionDetails[pathName].title} description={heroSectionDetails[pathName].description} />
+      <AlumniSection />
+      <IndustrySection />
+      <CurriculamSection />
+      <LearnerSection />
+      {/* <EventSection /> */}
+      <FAQSection />
+    </div>
+  );
+}
